e02cc6f2e9e20f25aa11a3b562cc2e7d55583d53
Hermann Hueck <hermann.hueck@mailbox.org>
2020-07-13 11:16:34 +0200

    upgrade silencer plugin/lib: 1.6.0 -> 1.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -57,7 +57,7 @@ object Dependencies {
   lazy val singletonOps       = "eu.timepit"              %% "singleton-ops"               % singletonOpsVersion
 
   // compilerPlugins
-  lazy val silencerVersion         = "1.6.0"
+  lazy val silencerVersion         = "1.7.0"
   lazy val kindProjectorVersion    = "0.11.0"
   lazy val betterMonadicForVersion = "0.3.1"
   lazy val macroParadiseVersion    = "2.1.1"
